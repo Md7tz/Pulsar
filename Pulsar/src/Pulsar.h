473dfd60c069c661b046452ddfc2edceef92d7e5
Medhat Usama <medhatusama@gmail.com>
2022-03-25 23:57:39 +0300

    Added ImGui and ImGui layer

--- Pulsar/src/Pulsar.h
+++ Pulsar/src/Pulsar.h
@@ -4,6 +4,7 @@
 #include "Pulsar/Application.h"
 #include "Pulsar/Layer.h"
 #include "Pulsar/Log.h"
+#include "Pulsar/ImGui/ImGuiLayer.h"
 
 // ------Entry Point------
 #include "Pulsar/EntryPoint.h"
